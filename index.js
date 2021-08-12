f9c12c02a5045e7535e49f0e169ebb697099d32e
Jose <jfgallegocardona@gmail.com>
2021-08-11 23:26:38 -0500

    Creando las tarjetas con la TODO

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 let input = document.getElementById('inputText');
 let button = document.getElementById('inputButton');
+let todoContainer = document.getElementById('todoContainer')
+
+function pintar(todo){
+    todoContainer.innerHTML = todo
+}
+
+//Enviar la informacion al formulario para agregarla en la todoapp
+button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log(input.value)
+    let todoValue = input.value
+    //Imprimir una tarjeta con la tarea
+    const article =`
+        <article id="todo">
+        <span>${todoValue}</span>
+        <br>
+        <label for="completedTask">Completed</label>
+        <input type="radio" name="Completed" id="completedTask">
+        <br>
+        <label for="remainingTask">Remaining</label>
+        <input type="radio" name="Remaining" id="remainingTask">
+        </article>
+`
+    pintar(article)
+})
+
 
