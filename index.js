332acfb2dbd82fc8903c55c88011a8485d03b5aa
Jose Fernando <dev@pop-os.localdomain>
2021-09-01 21:05:50 -0500

    Creacion de tarjeta de tareas listo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let button = document.getElementById('inputButton');
 let todoContainer = document.getElementById('todoContainer')
 
 function pintar(todo){
-    todoContainer.innerHTML = todo
+    todoContainer.insertAdjacentHTML("beforeend",todo)
 }
 
 //Enviar la informacion al formulario para agregarla en la todoapp
@@ -13,14 +13,14 @@ button.addEventListener('click', (e)=>{
     let todoValue = input.value
     //Imprimir una tarjeta con la tarea
     const article =`
-        <article id="todo">
+        <article class="todo">
         <span>${todoValue}</span>
         <br>
         <label for="completedTask">Completed</label>
-        <input type="radio" name="Completed" id="completedTask">
+        <input type="radio" name="${todoValue}">
         <br>
         <label for="remainingTask">Remaining</label>
-        <input type="radio" name="Remaining" id="remainingTask">
+        <input type="radio" name="${todoValue}">
         </article>
 `
     pintar(article)
